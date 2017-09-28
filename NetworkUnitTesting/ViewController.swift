9283457e95fcb87a7ab287872f57f75171237f25
Dinh Vu Nam <namdinh@hiworld.com.vn>
2017-09-28 16:31:32 +0700

    Initial Commit

---
+++ NetworkUnitTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetworkUnitTesting
+//
+//  Created by Dinh Vu Nam on 9/28/17.
+//  Copyright © 2017 NamDV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
